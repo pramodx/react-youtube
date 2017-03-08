b9a1ed8725caabdc285d9d9f7eb2dff469cf5847
Pramod Nair <pramod.nair@emprevo.com>
2017-03-08 22:19:23 +1100

    youtube search app done

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -10,10 +10,15 @@ export default class SearchBar extends Component {
     render() {
         return (
             <div>
-                <input onChange={ e => this.setState({ term: e.target.value }) } />
-                Value of the input { this.state.term }
+                <input value={ this.state.term } onChange={ e => this.onInputChange(e.target.value) } />
+
             </div>
         );
+    }
+
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
    
 }
